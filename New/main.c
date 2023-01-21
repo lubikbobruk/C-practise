f1db07cd27cdebad8162801b7a62d0eabf936743
Lubomyr <bobroliu@fit.cvut.cz>
2023-01-21 02:13:26 +0100

    Fibonacci code

--- New/main.c
+++ New/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+int Fibonacci(int cycles)
+{
+    int first=1,second=1,current;
+    printf("1 1 ");
+    for(int i=0;i<cycles-1;i++)
+    {
+        current = first + second;
+        printf("%d ",current);
+        first =second;
+        second = current;
+
+    }
+
+}
+
 int main()
 {
+    Fibonacci(10);
 
 }
