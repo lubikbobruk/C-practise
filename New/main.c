2731a81d1441109accc9b236c931c64d16e5aaa7
Lubomyr <bobroliu@fit.cvut.cz>
2023-01-21 21:50:40 +0100

    7.3

--- New/main.c
+++ New/main.c
@@ -1,65 +1,74 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-//#define FIRST_TASK
-//#define SECOND_TASK
-//#define THIRD_TASK
-#ifdef FIRST_TASK
-int main()
-{
- int n;
- printf("Please enter a size of array");
- if(scanf("%d",&n)!=1)
-     return 1;
- int* array = calloc(n,sizeof(*array));
- for(int i=0;i<n;i++)
-     printf("%d",array[i]);
-free(array);
-    return 0;
-}
-#endif
 
-#ifdef SECOND_TASK
+int areEqual(char * arr1,char * arr2,int n)
+{
+    if (strncmp(arr1,arr2,n)==0)
+        return 1;
+    else
+        return 0;
+}
+void reverse(char * newarr,int length)
+{
+    char temp[length+1];
+    strncpy(temp,newarr,length);
+    printf("temp - %s",temp);
+    for(int i=0;i<length-1;i++)  //              temp={'r','u','n','\n','\0'}   newarr={'n','u','r','\n','\0','\0',...} strlen= 4
+    {
+        newarr[i] = temp[length - i - 2];
+    }
+}
+
+
+
+
 int main()
 {
     int n;
-    printf("Please enter a size of array");
-    if(scanf("%d",&n)!=1)
+    printf("Enter the size of a string: ");
+    if(scanf("%d",&n) != 1)
+    {
+        printf("Invalid input.\n");
         return 1;
-    int* array =malloc(n*sizeof(*array));
-    for(int i=0;i<n;i++)
-    {
-        array[i] = i+1;
-        printf("%d ", array[i]);
     }
+    getchar();//removes newline from the buffer!!!
+    n++;
 
-    return 0;
-}
-#endif
-
-#ifdef THIRD_TASK
-
-int main()
-{
-    int a,n;
-    printf("Please enter a size of an array.\n");
-    if(scanf("%d",&n)!=1){
-        printf("Invalid input.\n");
-        return 1;}
-    printf("Enter the value of the first element of an array.\n");
-    if(scanf("%d",&a)!=1) {
-        printf("Invalid input.\n");
+    char *arr=(char *)calloc(n,sizeof(arr));
+    printf("Enter a string : ");
+    if(fgets (arr, n, stdin)==NULL)
         return 2;
+    puts(arr);
+
+    printf("Enter a second string : ");
+
+    char *newarr = (char*)calloc(n,sizeof(arr));
+    if(fgets (newarr, n, stdin)==NULL)
+        return 3;
+    puts(newarr);
+    puts("Reversing...");
+    if(strlen(newarr)==strlen(arr))
+        reverse(newarr,strlen(newarr));
+    else {
+        puts("strings are not of the same height!\n");
+        return 4;
     }
+    puts(newarr);
+    printf("Are %s and %s equal?\n",arr,newarr);
 
 
-    int* array =malloc(n*sizeof(*array));
-    for(int i=0;i<n;i++,a++)
-    {
-        array[i] = a;
-        printf("%d ", array[i]);
-    }
+    printf("\nstrlen arr - %d\n",strlen(arr));
+    printf("\nstrlen newarr - %d\n",strlen(newarr));
+    if(areEqual(arr, newarr,strlen(arr)))
+        puts("yes");
+    else
+        puts("no");
+
+    free(arr);
+    free(newarr);
 
     return 0;
+
 }
-#endif
 
